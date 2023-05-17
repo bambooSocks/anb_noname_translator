9587cd1dc7c28c34caaa6fcbf2612aab8742e706
Matej Majtan <majtan.matej@gmail.com>
2023-05-17 14:23:24 +0200

    Added check for deduction

--- Verification.hs
+++ Verification.hs
@@ -1,45 +1,51 @@
 module Verification where
 
 import qualified Control.Monad.Trans.State as ST
+import qualified Data.Map as M
 
 data Msg
   = Atom String
-  | Composition String [Msg]
+  | Comp String [Msg]
   deriving (Show, Eq)
 
 type Frame = [(Msg, Msg)]
+type AgentFrames = M.Map String Frame
 
-data FrameState = FrameState
+data State = State
   { counter :: Int
   , frame   :: Frame
+  , frames  :: AgentFrames -- TODO: convert the rest to support agent specific frames
+  , pubFunc :: [String]
   }
   deriving (Show)
 
-initialFrameState :: FrameState
-initialFrameState = FrameState 
+initialFrameState :: State
+initialFrameState = State 
   { counter = 0
-  , frame = []
+  , frame   = []
+  , frames  = M.fromList []
+  , pubFunc = ["pair", "crypt", "scrypt", "sign"]
   }
 
-type MState = ST.StateT FrameState []
+type MState = ST.StateT State []
 
-get :: MState FrameState
+get :: MState State
 -- fetch the current state
 get = ST.get
 
-put :: FrameState -> MState ()
+put :: State -> MState ()
 -- update the state with new version
 put = ST.put
 
-runMState :: MState a -> FrameState -> [(a, FrameState)]
+runMState :: MState a -> State -> [(a, State)]
 -- apply the state transformer on an state and return the final state and value
 runMState = ST.runStateT
 
-evalMState :: MState a -> FrameState -> [a]
+evalMState :: MState a -> State -> [a]
 -- apply the state transformer on an state and return the final value
 evalMState = ST.evalStateT
 
-execMState :: MState a -> FrameState -> [FrameState]
+execMState :: MState a -> State -> [State]
 -- apply the state transformer on an state and return the final state
 execMState = ST.execStateT
 
@@ -73,43 +79,64 @@ receiveIfInFrame cond_msg rcv_msg = do
 
 receive :: Msg -> MState Int
 -- Atom receiving
-receive m@(Atom id) = do
+receive m@(Atom _) = do
     fresh m
 -- pair receiving
-receive m@(Composition "pair" (f:s:[])) = do
+receive m@(Comp "pair" (f:s:[])) = do
     fresh m
     receive f
     receive s
 -- scrypt receiving has to have a key sk in frame to receive the msg
-receive m@(Composition "scrypt" (sk:msg:[])) = do
+receive m@(Comp "scrypt" (sk:msg:[])) = do
     fresh m
     receiveIfInFrame sk msg
 -- crypt receiving has to have a private key inv(pk) in frame to receive the msg
-receive m@(Composition "crypt" (pk:msg:[])) = do
+receive m@(Comp "crypt" (pk:msg:[])) = do
     fresh m
-    receiveIfInFrame (Composition "inv" [pk]) msg
+    receiveIfInFrame (Comp "inv" [pk]) msg
 -- sign receiving has to have a public key pk in frame to receive the msg
-receive m@(Composition "sign" ((Composition "inv" (pk:[])):msg:[])) = do
+receive m@(Comp "sign" ((Comp "inv" (pk:[])):msg:[])) = do
     fresh m
     receiveIfInFrame pk msg
 -- fallback receive for other compositions
-receive m@(Composition id args) = -- TODO: support custom deconstructors
+receive m@(Comp id args) = -- TODO: support custom deconstructors
     fresh m
+
+canDeduceFromFrame :: Msg -> MState Bool
+-- checks whether an atom can be deduced from the frame
+canDeduceFromFrame msg@(Atom _) = do
+  state <- get
+  let fr = map snd (frame state)
+  return (any (msg ==) fr)
+-- checks whether a composition can be deduced from the frame
+canDeduceFromFrame msg@(Comp id args) = do
+  state <- get
+  let pubFuncs = pubFunc state
+  let fr = map snd (frame state)
+  let isInFrame =  (any (msg ==) fr)
+  let idAllowed = any (id ==) pubFuncs
+  -- TODO: clean this up
+  let allArgsCanBeDeduced = all (\a -> all (True ==) (evalMState (canDeduceFromFrame a) state)) args
+  return (isInFrame || (idAllowed && allArgsCanBeDeduced))
 
 main :: IO ()
 main = do
   ---- pair example
-  -- let x = Composition "pair" [Atom "a", Composition "pair" [Atom "b", Atom "c"]]
+  -- let x = Comp "pair" [Atom "a", Comp "pair" [Atom "b", Atom "c"]]
   ---- scrypt example
-  -- let x = Composition "pair" [Atom "sk", (Composition "scrypt" [Atom "sk", Atom "secret"])]
-  let x = Composition "pair" [(Composition "scrypt" [Atom "sk", Atom "secret"]), Atom "sk"]
-  -- let x = Composition "scrypt" [Atom "sk", Atom "secret"]
+  -- let x = Comp "pair" [Atom "sk", (Comp "scrypt" [Atom "sk", Atom "secret"])]
+  -- let x = Comp "pair" [(Comp "scrypt" [Atom "sk", Atom "secret"]), Atom "sk"]
+  -- let x = Comp "scrypt" [Atom "sk", Atom "secret"]
   ---- crypt example
-  -- let x = Composition "pair" [Composition "inv" [Atom "pk"], (Composition "crypt" [Atom "pk", Atom "secret"])]
-  -- let x = Composition "crypt" [Atom "pk", Atom "secret"]
+  -- let x = Comp "pair" [Comp "inv" [Atom "pk"], (Comp "crypt" [Atom "pk", Atom "secret"])]
+  -- let x = Comp "crypt" [Atom "pk", Atom "secret"]
   ---- sign example
-  -- let x = Composition "pair" [Atom "pk", (Composition "sign" [Composition "inv" [Atom "pk"], Atom "secret"])]
-  -- let x = Composition "sign" [Composition "inv" [Atom "pk"], Atom "secret"]
+  -- let x = Comp "pair" [Atom "pk", (Comp "sign" [Comp "inv" [Atom "pk"], Atom "secret"])]
+  -- let x = Comp "sign" [Comp "inv" [Atom "pk"], Atom "secret"]
+  ---- test for deducing scrypt(sk, secret)
+  let x = Comp "pair" [Atom "secret", Atom "sk"]
   let mst = receive x
-  let st = execMState mst initialFrameState
+  let st:[] = execMState mst initialFrameState
+  let canDeduce = evalMState (canDeduceFromFrame (Comp "scrypt" [Atom "sk", Atom "secret"])) st
   putStrLn $ show st
+  putStrLn $ show canDeduce
